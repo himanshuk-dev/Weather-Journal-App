dc1e4c9ebf7ca98bbfe2cd2eedd99089389cf7cf
himanshukumar30 <himanshu.kumar2@outlook.com>
2022-04-18 22:45:08 -0700

    setup project environment

---
+++ server.js
@@ -0,0 +1,45 @@
+// Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+// Dependencies
+const bodyParser = require("body-parser");
+/*Middleware*/ 
+
+// Here we are configuring express to use body-parser as middle-ware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static("website"));
+
+
+const port = 5000;
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log("server running");
+    console.log(`running on localhost: ${port}`);
+}
+
+// Capture weather data
+const appData = []
+app.post('/addEntry', addEntry)
+
+function addEntry(req,res){
+
+    newEntry = {
+      temperature: req.body.temperature,
+      date: req.body.date,
+      comments: req.body.comments
+    }
+  
+    addEntry.push(newEntry)
+    console.log(addEntry)
+  }
