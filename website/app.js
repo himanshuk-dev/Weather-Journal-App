e2e0248b71596ad46de2b25b20b63532c2b08d7c
himanshukumar30 <himanshu.kumar2@outlook.com>
2022-04-19 23:27:37 -0700

    added code for get webb API data

--- website/app.js
+++ website/app.js
@@ -3,12 +3,28 @@ let baseURL = 'api.openweathermap.org/data/2.5/weather';
 let apiKey = '76aa91139c7537d1992cc3e86268bd28';
 
 // Event listener to add function to existing HTML DOM element
-
+document.getElementById('go').addEventListener('click', taskToPerform);
 
 /* Function called by event listener */
-
+function taskToPerform(e){
+    const location =  document.getElementById('zipCode').value;
+    getLocation(baseURL,location, apiKey)
+    }
 
 /* Function to GET Web API Data*/
+const getLocation = async (baseURL, zipCode, key)=>{
+
+    const res = await fetch(baseURL+zipCode+key)
+    try {
+  
+      const appData = await res.json();
+      console.log(appData)
+      return appData;
+    }  catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+  }
 
 /* Function to POST data */
 
