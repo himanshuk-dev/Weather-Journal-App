2aef13aca6805c94e6012f7b4aa4de3596f33b8a
himanshukumar30 <himanshu.kumar2@outlook.com>
2022-04-23 16:09:00 -0700

    added code to POST data

--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 // Personal API Key for OpenWeatherMap API
-let baseURL = 'api.openweathermap.org/data/2.5/weather';
+let baseURL = 'api.openweathermap.org/data/2.5/weather/';
 let apiKey = '76aa91139c7537d1992cc3e86268bd28';
 
 // Event listener to add function to existing HTML DOM element
@@ -8,13 +8,14 @@ document.getElementById('go').addEventListener('click', taskToPerform);
 /* Function called by event listener */
 function taskToPerform(e){
     const location =  document.getElementById('zipCode').value;
-    getLocation(baseURL,location, apiKey)
+    const comments =  document.getElementById('comments').value;
+    getData(baseURL,location, apiKey)
     }
 
 /* Function to GET Web API Data*/
-const getLocation = async (baseURL, zipCode, key)=>{
+const getData = async (baseURL, location, key)=>{
 
-    const res = await fetch(baseURL+zipCode+key)
+    const res = await fetch(baseURL+location+key)
     try {
   
       const appData = await res.json();
@@ -27,6 +28,29 @@ const getLocation = async (baseURL, zipCode, key)=>{
   }
 
 /* Function to POST data */
+const postData = async (url = '', appData = {}) =>{
+  const res = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(
+      temperature: appData.temperature, 
+      date: appData.date, 
+      comments: appData.comments
+    )
+  });
+  try {
+  
+    const newData = await res.json();
+    console.log(newData)
+    return newData;
+  }  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+}
 
 
 /* Function to GET Project Data */
