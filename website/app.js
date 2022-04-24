e081cc3df2277479eb4d327e302433433aa152ad
himanshukumar30 <himanshu.kumar2@outlook.com>
2022-04-24 12:27:08 -0700

    added code to enter country code

--- website/app.js
+++ website/app.js
@@ -1,21 +1,35 @@
+// Create a new date instance dynamically with JS
+let d = new Date();
+// let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
 // Personal API Key for OpenWeatherMap API
-let baseURL = 'api.openweathermap.org/data/2.5/weather/';
-let apiKey = '76aa91139c7537d1992cc3e86268bd28';
+let baseURL = 'api.openweathermap.org/data/2.5/weather/?zip=';
+let apiKey = '&appID=76aa91139c7537d1992cc3e86268bd28';
 
 // Event listener to add function to existing HTML DOM element
 document.getElementById('go').addEventListener('click', taskToPerform);
 
 /* Function called by event listener */
 function taskToPerform(e){
-    const location =  document.getElementById('zipCode').value;
-    const comments =  document.getElementById('comments').value;
-    getData(baseURL,location, apiKey)
-    }
+  const location =  document.getElementById('zipCode').value;
+  const country =  document.getElementById('country').value;
+  const comments =  document.getElementById('comments').value;
+
+  getData(baseURL,location, apiKey)
+
+    .then(function(data){
+        console.log(data);
+        postData('/add', {date:d, temperature:data.temperature, comments: data.comments});
+        updateUI();
+    })
+};
 
 /* Function to GET Web API Data*/
-const getData = async (baseURL, location, key)=>{
+const getData = async (baseURL, location, apiKey)=>{
 
-    const res = await fetch(baseURL+location+key)
+  // http://127.0.0.1:5500/Projects/Weather-Journal-App/website/api.openweathermap.org/data/2.5/weather/V6B1B47/6aa91139c7537d1992cc3e86268bd28
+    const res = await fetch(baseURL+location+apiKey)
     try {
   
       const Data = await res.json();
