54e9341cbf759a6da552c3927477b8bffe9cdb79
himanshukumar30 <himanshu.kumar2@outlook.com>
2022-04-27 00:43:55 -0700

    fixed errors in get

--- website/app.js
+++ website/app.js
@@ -16,25 +16,36 @@ function taskToPerform(e){
   const country =  document.getElementById('country').value;
   const comments =  document.getElementById('feelings').value;
 
-  getData(baseURL,location,country, apiKey)
+  getData(baseURL,location,country,apiKey)
 
     .then(function(data){
-        console.log(data);
-        postData('/add', {date:newDate, temperature:data.list[0].main.temp, comments: feelings});
+        console.log("The data is:", data);
+        console.log("The temperature is:", data.main.temp)
+        console.log("The feelings are:", feelings)
+        console.log("The date is:", newDate)
+        postData('/add', {date:newDate, temperature:data.main.temp, comments: feelings})
         updateUI();
     })
 };
 
 // Function to GET Web API Data (Get Route II)
-const getData = async (baseURL, location,country, apiKey)=>{
+const getData = async (baseURL,location,country,apiKey)=>{
 
   // http://127.0.0.1:5500/Projects/Weather-Journal-App/website/api.openweathermap.org/data/2.5/weather/V6B1B47/6aa91139c7537d1992cc3e86268bd28
-    const res = await fetch(baseURL+location,country+apiKey)
+    
+  // fetch(input: RequestInfo, init?: RequestInit): Promise<Response>;
+  const url = "http://"+baseURL+location+","+country+apiKey;
+  // https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
+  // api.openweathermap.org/data/2.5/weather/?zip=121001,in&appID=76aa91139c7537d1992cc3e86268bd28
+  console.log("the url is:", url);
+  console.log("calling fetch")
+  const res = await fetch(url)
+  console.log("Fetch call completed")
     try {
   
-      const Data = await res.json();
-      console.log(Data);
-      return Data;
+      const data = await res.json();
+      console.log(data);
+      return data;
     }  catch(error) {
       console.log("error", error);
       // appropriately handle the error
@@ -42,7 +53,8 @@ const getData = async (baseURL, location,country, apiKey)=>{
   }
 
 /* Function to POST data */
-const postData = async (url = '', Data = {}) =>{
+const postData = async (url = '', data = {}) =>{
+  console.log("The url in he post is:", url)
   const res = await fetch(url, {
     method: 'POST',
     credentials: 'same-origin',
@@ -63,7 +75,7 @@ const postData = async (url = '', Data = {}) =>{
 }
 
 /* Function to GET Project Data */
-const retrieveData = async () =>{
+const updateUI = async () =>{
   const request = await fetch('/all');
   try {
   // Transform into JSON
