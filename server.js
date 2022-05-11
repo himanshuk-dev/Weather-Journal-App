f36b640584567d8e2c77f1487a7a49c9cea5e46d
Himanshu Kumar <himanshu.kumar2@outlook.com>
2022-05-10 23:09:29 -0700

    formatted code

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const projectData = {
   temp: "",
-  userResponse:"",
-  date:""
+  userResponse: "",
+  date: "",
 };
 
 // Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
 
 // Dependencies
 const bodyParser = require("body-parser");
-/*Middleware*/ 
 
 // Here we are configuring express to use body-parser as middle-ware
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
@@ -29,29 +28,34 @@ app.use(express.static("website"));
 const port = 8888;
 const server = app.listen(port, listening);
 
-function listening(){
-    console.log("server running");
-    console.log(`running on localhost: ${port}`);
+function listening() {
+  console.log("server running");
+  console.log(`running on localhost: ${port}`);
 }
 
-// Capture weather data (Get Route I)
-app.get('/retreiveData',retreiveData);
+/**
+ * Post route to store data coming from the client
+ */
+app.post("/sendData", storeData);
 
-function retreiveData(req,res) {
-  console.log('Data being sent:', projectData)
-  res.send(projectData);
+/**
+ * Get route to send data back to the client (Get Route I)
+ */
+ app.get("/retreiveData", retreiveData);
 
-}
-
-// Post weather data
-app.post('/sendData', sendData)
-async function sendData(req,res){
-  console.log('Received data from client, data:', req.body);
+/**
+ * Function to store data coming from the client and responding with statusCode
+ */
+async function storeData(req, res) {
   projectData.temp = req.body.temp;
   projectData.userResponse = req.body.userResponse;
   projectData.date = req.body.date;
-  console.log('data assigned at post route:', projectData)
-  const data = await req.body;
-  console.log('Sending response status code to client');
   res.status(200).send();
+}
+
+/**
+ * Function to send data back to the client
+ */
+function retreiveData(req, res) {
+  res.send(projectData);
 }
--- website/app.js
+++ website/app.js
@@ -1,145 +1,143 @@
 // Create a new date instance dynamically with JS
 let today = new Date();
-let newDate = today.getFullYear() + '-' +(today.getMonth()+1 )+ '-' + today.getDate();
-console.log("Date is:", newDate);
+let newDate =
+  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
 
-
-// Personal API Key for OpenWeatherMap API
-const weatherServerURL = 'https://api.openweathermap.org/data/2.5/weather/?zip=';
-const apiKey = '&appID=76aa91139c7537d1992cc3e86268bd28';
-const localhostServerURL = 'http://localhost:8888';
+const weatherServerURL = "https://api.openweathermap.org/data/2.5/weather/";
+const apiKey = "76aa91139c7537d1992cc3e86268bd28"; // Personal API Key for OpenWeatherMap API
+const localhostServerURL = "http://localhost:8888";
+const postMethodName = "/sendData";
+const getMethodName = "/retreiveData";
 
 // Event listener to add function to existing HTML DOM element
-document.getElementById("generate").addEventListener('click', taskToPerform);
+document
+  .getElementById("generate")
+  .addEventListener("click", fetchWeatherDataAndDisplay);
 
 /* Function called by event listener */
-function taskToPerform(){
-  const location =  document.getElementById('zip').value;
-  const country =  document.getElementById('country').value;
-  const userResponse =  document.getElementById('feelings').value;
+function fetchWeatherDataAndDisplay() {
+  const zipcode = document.getElementById("zip").value;
+  const countryCode = document.getElementById("countryCode").value;
+  const userResponse = document.getElementById("feelings").value;
 
-  getData(weatherServerURL,location,country,apiKey)
-  .then((data) =>{
-    console.log("The data is:", data);
-    console.log("The temperature is:", data.main.temp)
-    console.log("The feelings are:", userResponse)
-    console.log("The date is:", newDate)
-  
-    prepareData(data, userResponse)
-    .then((projectData)=>{
-      console.log('Data prepared to be sent to server:', projectData)
-      postData(localhostServerURL + '/sendData', projectData)
-      .then((responseData)=>{
-        console.log('Response from server:', responseData)
-        if(responseData.status == 200){
-          retrieveData(localhostServerURL + '/retreiveData')
-          .then((uiData)=>{
-            console.log('Data to be displayed in UI:', uiData)
-            updateUI(uiData);
-          })
-        } else{
-          // Displaying in UI that there was some error.
-          console.log("error in the api call");
-        }
-   
-      })
-    })
-  });
-};
-
-// Function to GET Web API Data (Get Route II)
-const getData = async (baseURL,location,country,apiKey)=>{
-
-  const url = weatherServerURL + location + "," + country + apiKey;
-  console.log("Calling weather server api with url:", url);
-
-  const res = await fetch(url)
-  console.log("Fetch call completed")
-    try {
-  
-      const data = await res.json();
-      console.log("Received data from weatherserver", data);
-      return data;
-    }  catch(error) {
-      console.log("error", error);
-      // appropriately handle the error
+  getWeatherDataByZipcode(weatherServerURL, zipcode, countryCode, apiKey).then(
+    (weatherData) => {
+      prepareData(weatherData, userResponse).then((projectData) => {
+        postData(localhostServerURL + postMethodName, projectData).then(
+          (responseData) => {
+            if (responseData.status == 200) {
+              retrieveData(localhostServerURL + getMethodName).then(
+                (uiData) => {
+                  updateUI(uiData);
+                }
+              );
+            } else {
+              console.log("Error sending data to local server.");
+            }
+          }
+        );
+      });
     }
-  }
-
-// Function to prepare project data that need to be sent to server
-const prepareData = async (data, userResponse) =>{
-  try{
-    if(data.message){
-      // Error case
-      console.log('Error response:', data)
-      const projectData = data.message;
-      console.log('Error case project data:', projectData) 
-      return projectData;    
-    } else {
-      // success case
-      console.log('Successful response:', data)
-      const projectData = {
-        temp: data.main.temp,
-        userResponse: userResponse,
-        date: newDate
-      };
-      console.log('Successful case project data:', projectData)
-      return projectData;
-    }
-  }catch(error){
-    console.error(error);
-  }
+  );
 }
 
-/* Function to POST data */
-const postData = async (url = '', projectData = {}) =>{
-  console.log("postData called with url:", url)
-  console.log('postData called with projectData:', projectData)
-  
+/**
+ * Function to GET Web API Data (Get Route II)
+ */
+const getWeatherDataByZipcode = async (
+  weatherServerURL,
+  zipcode,
+  countryCode,
+  apiKey
+) => {
+  const url =
+    weatherServerURL +
+    "?zip=" +
+    zipcode +
+    "," +
+    countryCode +
+    "&appID=" +
+    apiKey;
+  const response = await fetch(url);
+  try {
+    const weatherData = await response.json();
+    return weatherData;
+  } catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
+
+/**
+ * Function to prepare project data that need to be sent to local server
+ */
+const prepareData = async (weatherData, userResponse) => {
+  try {
+    if (weatherData.message) {
+      // Error case
+      const projectData = weatherData.message;
+      return projectData;
+    } else {
+      // Success case, create projectData
+      const projectData = {
+        temp: weatherData.main.temp,
+        userResponse: userResponse,
+        date: newDate,
+      };
+      return projectData;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * Function to POST projectData
+ */
+const postData = async (url = "", projectData = {}) => {
   try {
     const response = await fetch(url, {
-      method: 'POST',
-      credentials: 'same-origin',
+      method: "POST",
+      credentials: "same-origin",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(projectData),
     });
-    console.log('postData completed with response:', response.status)
     return response;
-  }  catch(error) {
+  } catch (error) {
     console.log("error", error);
     // appropriately handle the error
   }
-}
+};
 
-/* Function to GET Project Data */
-const retrieveData = async(url) =>{
-  console.log("retrieveData called with url:", url)
-  const data = await fetch(url)
-    try {
-      const result = await data.json();
-      console.log('Data received from server through /retrieveData route:',result);
-      return result;
-    }  catch(error) {
-      console.log("error", error);
-      // appropriately handle the error
-    }
-}
-
-const updateUI = async (uiData) =>{
-  const request = await fetch('http://localhost:8888/retreiveData');
+/**
+ * Function to GET Project Data
+ */
+const retrieveData = async (url) => {
+  const data = await fetch(url);
   try {
-  // Transform into JSON
-    const uiData = await request.json()
-    console.log('updateUI called with:', uiData)
-  // Write updated data to DOM elements
-    document.getElementById('date').innerHTML = uiData.date;
-    document.getElementById('temperature').innerHTML = uiData.temp;
-    document.getElementById('userResponse').innerHTML = uiData.userResponse
-  }
-  catch(error) {
+    const result = await data.json();
+    return result;
+  } catch (error) {
     console.log("error", error);
     // appropriately handle the error
   }
-}
+};
+
+/**
+ * Function to update UI with output of API calls
+ */
+const updateUI = async (uiData) => {
+  const request = await fetch("http://localhost:8888/retreiveData");
+  try {
+    const uiData = await request.json();
+    // Write updated data to DOM elements
+    document.getElementById("date").innerHTML = uiData.date;
+    document.getElementById("temperature").innerHTML = uiData.temp - 273.15 + " " + "C"; //C is for denoting temperature in Celcius
+    document.getElementById("userResponse").innerHTML = uiData.userResponse;
+  } catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
